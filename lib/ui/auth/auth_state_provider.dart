8ae37382184b01a03370955ea2a25ae086b98193
dl10yr <tkzw04202166@gmail.com>
2025-02-24 12:37:45 +0900

    auth_state_provider

--- lib/ui/auth/auth_state_provider.dart
+++ lib/ui/auth/auth_state_provider.dart
@@ -6,7 +6,9 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:flutter_my_blueprint/data/repositories/auth/auth_repository.dart';
 import 'package:flutter_my_blueprint/domain/model/auth/auth_state.dart';
 
+part 'auth_state_provider.g.dart';
+
 @riverpod
 AuthState authState(Ref ref) {
-  return ref.watch(remoteAuthRepositoryProvider);
+  return ref.watch(remoteAuthRepositoryProvider).authState;
 }
