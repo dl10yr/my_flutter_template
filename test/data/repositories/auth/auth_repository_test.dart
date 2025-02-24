226835c036fd1014c6c1f7ac91fa728adf3ba4c9
dl10yr <tkzw04202166@gmail.com>
2025-02-24 22:16:34 +0900

    chore: import_sorter

--- test/data/repositories/auth/auth_repository_test.dart
+++ test/data/repositories/auth/auth_repository_test.dart
@@ -1,10 +1,10 @@
-
-import 'package:flutter_my_blueprint/data/repositories/auth/auth_repository.dart';
-import 'package:flutter_my_blueprint/domain/model/auth/auth_state.dart';
+// Package imports:
 import 'package:flutter_test/flutter_test.dart';
 
+// Project imports:
+import 'package:flutter_my_blueprint/data/repositories/auth/auth_repository.dart';
+import 'package:flutter_my_blueprint/domain/model/auth/auth_state.dart';
 import '../../../../testing/fakes/data/services/fake_github_token_service.dart';
-
 
 void main() {
   group('AuthRepositoryRemote tests', () {
