95a6fbe507f610e4c55c96bd3d78c8d2f5329488
dl10yr <tkzw04202166@gmail.com>
2025-04-13 10:46:03 +0900

    layer-firstの削除とvscodeディレクトリの移動

--- layer-first/lib/data/services/shared_preference/app_shared_prefernce_key.dart
+++
@@ -1,6 +0,0 @@
-class AppSharedPreferenceKey<T> {
-  const AppSharedPreferenceKey(this.keyName);
-  final String keyName;
-
-  static const favofites = AppSharedPreferenceKey<List<String>>('favorites');
-}
--- layer-first/lib/domain/usecase/usecase.dart
+++
@@ -1,5 +0,0 @@
-// ref: https://developers.cyberagent.co.jp/blog/archives/36149/
-
-abstract class UseCase<Param, Result> {
-  Result call(Param param);
-}
