457cac88e03015a4bf0c25442b621a088b7184ba
dl10yr <tkzw04202166@gmail.com>
2025-04-20 20:15:36 +0900

    listen appException in main.dart

--- apps/my_app/lib/main.dart
+++ apps/my_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cores_core/custom_hooks/use_effect_once.dart';
 import 'package:cores_core/data/service/shared_preference/app_shared_prefernce_key.dart';
+import 'package:cores_core/exception/provider/app_exception_notifier.dart';
 import 'package:cores_core/extension/enum_extension.dart';
 import 'package:cores_core/logger/logger.dart';
 import 'package:cores_core/provider/app_lifecycle_notifier.dart';
@@ -13,8 +14,8 @@ import 'package:cores_core/widgets/snack_bar/app_snack_bar_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:my_app/routing/go_router_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:my_app/routing/go_router_provider.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -49,20 +50,30 @@ class MyApp extends HookConsumerWidget {
       return null;
     });
 
-    ref.listen(appLifecycleNotifierProvider, (_, newState) {
-      switch (newState) {
-        case AppLifecycleState.resumed:
-          logger.d('AppLifecycleState.resumed');
-        case AppLifecycleState.inactive:
-          logger.d('AppLifecycleState.inactive');
-        case AppLifecycleState.paused:
-          logger.d('AppLifecycleState.paused');
-        case AppLifecycleState.detached:
-          logger.d('AppLifecycleState.detached');
-        case AppLifecycleState.hidden:
-          logger.d('AppLifecycleState.hidden');
-      }
-    });
+    ref
+      ..listen(appLifecycleNotifierProvider, (_, newState) {
+        switch (newState) {
+          case AppLifecycleState.resumed:
+            logger.d('AppLifecycleState.resumed');
+          case AppLifecycleState.inactive:
+            logger.d('AppLifecycleState.inactive');
+          case AppLifecycleState.paused:
+            logger.d('AppLifecycleState.paused');
+          case AppLifecycleState.detached:
+            logger.d('AppLifecycleState.detached');
+          case AppLifecycleState.hidden:
+            logger.d('AppLifecycleState.hidden');
+        }
+      })
+      ..listen(appExceptionNotifierProvider, (previous, next) {
+        if (next != null) {
+          ref.read(appExceptionNotifierProvider.notifier).consume();
+          AppSnackBarManager.showErrorSnackBar(
+            context: context,
+            message: next.message,
+          );
+        }
+      });
 
     final textTheme = createTextTheme(context);
 
