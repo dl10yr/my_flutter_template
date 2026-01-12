99050ab6d2054d3fde2c01754513f01585094d08
dl10yr <dl10yr@users.noreply.github.com>
2026-01-12 12:28:07 +0900

    chore: update Java and Kotlin versions in build configuration for compatibility

--- apps/my_app/android/app/build.gradle.kts
+++ apps/my_app/android/app/build.gradle.kts
@@ -11,13 +11,11 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
